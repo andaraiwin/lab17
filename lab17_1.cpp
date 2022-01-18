eccb24d42beed607171a36f79cc8bd6c4e657131
Raiwin Inthasit <andaraiwin@gmail.com>
2022-01-18 17:37:02 +0700

    Completed

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,39 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *a ,int N, int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << a[i];
+        if((i+1)%M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+
+void randData(double *a, int N, int M){
+    for(int i = 0; i < N*M; i++){
+        a[i] = (rand()%100)/100.0;
+    }
+}
+
+void findRowSum(const double *a,double *row,int N,int M) {
+    for (int i = 0; i < N; ++i) {
+        double sum = 0.0;
+        for (int j = 0; j < M; ++j){ 
+        sum += a[i*M + j];
+        }
+        row[i] = sum;
+    }
+}
+
+void findColSum(const double *a,double *col,int N,int M) {
+    for (int j = 0; j < M; ++j) {
+        double sum = 0.0;
+        for (int i = 0; i < N; ++i){
+        sum += a[i*M + j];
+        }
+        col[j] = sum;
+    }
+}
